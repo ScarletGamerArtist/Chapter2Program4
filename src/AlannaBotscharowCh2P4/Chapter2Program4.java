a0c6353e81e792429d024a7a9844948d98247c14
alanna.botscharow <alanna.botscharow@CLC-LAB209-18.TCSD.tooelesd.org>
2018-09-20 14:03:34 -0600

    Here is the table from the chapter 2 work sheet that I have recreated.

---
+++ src/AlannaBotscharowCh2P4/Chapter2Program4.java
@@ -0,0 +1,55 @@
+package AlannaBotscharowCh2P4;
+/* 
+ * Alanna Botscharow
+ * 9/18
+ * Chapter 2 Program 4
+ */
+public class Chapter2Program4 {
+
+	public static void main(String[] args) {
+		// Displays the table.
+		
+		
+		System.out.print("\t a");
+		System.out.print("|");
+		System.out.print("\t a^2");
+		System.out.print("|");
+		System.out.print("\t a^3");
+		System.out.print("|");
+		System.out.println();
+		System.out.println("       ------------------------");
+		System.out.print("\t 1");
+		System.out.print("|");
+		System.out.print("\t   1");
+		System.out.print("|");
+		System.out.print("\t   1");
+		System.out.print("|");
+		System.out.println();
+		System.out.println("       ------------------------");
+		System.out.print("\t 2");
+		System.out.print("|");
+		System.out.print("\t   4");
+		System.out.print("|");
+		System.out.print("\t   8");
+		System.out.print("|");
+		System.out.println(); 
+		System.out.println("       ------------------------");
+		System.out.print("\t 3");
+		System.out.print("|");
+		System.out.print("\t   9");
+		System.out.print("|");
+		System.out.print("\t  27");
+		System.out.print("|");
+		System.out.println();
+		System.out.println("       ------------------------");
+		System.out.print("\t 4");
+		System.out.print("|");
+		System.out.print("\t  16");
+		System.out.print("|");
+		System.out.print("\t  64");
+		System.out.print("|");
+		
+		
+		
+	}
+}
